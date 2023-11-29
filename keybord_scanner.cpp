a11224f3cef671155682097f0d52f22f475f51eb
UranvarosAsza <tom.halda@gmail.com>
2023-11-30 00:37:57 +0100

    Update keybord_scanner.cpp

--- keybord_scanner.cpp
+++ keybord_scanner.cpp
@@ -1,14 +1,13 @@
 #include "USB.h"
 #include "USBHIDKeyboard.h"
 #include <Arduino.h>
-//#define LAYOUT LAYOUT_HUN
+#define LAYOUT LAYOUT_HU //still not working 
 USBHIDKeyboard Keyboard;
 
-//ESP32 S2 Mini pins:
 int MyPinsInOrder[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 33, 34, 35, 36, 37, 38, 39, 40};
 int total = 27;
 
-void makeInputFromPin(int pin) // "go_z" in the original
+void makeInputFromPin(int pin) //eredetiben a go_z
 {
   pinMode(pin, INPUT_PULLUP);
   digitalWrite(pin, HIGH);
@@ -20,142 +19,143 @@ void makeOutputFromPin(int pin)
   digitalWrite(pin, LOW);
 }
 
-void readPinNumber(int button_num) {  // "usb_num" in the original
+void readPinNumber(int button_num) {  //eredetiben usb_num
   switch (button_num) {
     case 1:
-      Keyboard.press('1');
+      Keyboard.write('1');
       Keyboard.write(KEY_TAB);
       break;
     case 2:
-      Keyboard.press('2');
+      Keyboard.write('2');
       Keyboard.write(KEY_TAB);
       break;
     case 3:
-      Keyboard.press('3');
+      Keyboard.write('3');
       Keyboard.write(KEY_TAB);
       break;
     case 4:
-      Keyboard.press('4');
+      Keyboard.write('4');
       Keyboard.write(KEY_TAB);
       break;
     case 5:
-      Keyboard.press('5');
+      Keyboard.write('5');
       Keyboard.write(KEY_TAB);
       break;
     case 6:
-      Keyboard.press('6');
+      Keyboard.write('6');
       Keyboard.write(KEY_TAB);
       break;
     case 7:
-      Keyboard.press('7');
+      Keyboard.write('7');
       Keyboard.write(KEY_TAB);
       break;
     case 8:
-      Keyboard.press('8');
+      Keyboard.write('8');
       Keyboard.write(KEY_TAB);
       break;
     case 9:
-      Keyboard.press('9');
+      Keyboard.write('9');
       Keyboard.write(KEY_TAB);
       break;
     case 10:
-      Keyboard.press('1');
-      Keyboard.press('0');
+      Keyboard.write('1');
+      Keyboard.write('0');
       Keyboard.write(KEY_TAB);
       break;
     case 11:
-      Keyboard.press('1');
-      Keyboard.press('1');
+      Keyboard.write('1');
+      Keyboard.write('1');
       Keyboard.write(KEY_TAB);
       break;
     case 12:
-      Keyboard.press('1');
-      Keyboard.press('2');
+      Keyboard.write('1');
+      Keyboard.write('2');
       Keyboard.write(KEY_TAB);
       break;
     case 13:
-      Keyboard.press('1');
-      Keyboard.press('3');
+      Keyboard.write('1');
+      Keyboard.write('3');
       Keyboard.write(KEY_TAB);
       break;
     case 14:
-      Keyboard.press('1');
-      Keyboard.press('4');
+      Keyboard.write('1');
+      Keyboard.write('4');
       Keyboard.write(KEY_TAB);
       break;
     case 15:
-      Keyboard.press('1');
-      Keyboard.press('5');
+      Keyboard.write('1');
+      Keyboard.write('5');
       Keyboard.write(KEY_TAB);
       break;
     case 16:
-      Keyboard.press('1');
-      Keyboard.press('6');
+      Keyboard.write('1');
+      Keyboard.write('6');
       Keyboard.write(KEY_TAB);
       break;
     case 17:
-      Keyboard.press('1');
-      Keyboard.press('7');
+      Keyboard.write('1');
+      Keyboard.write('7');
       Keyboard.write(KEY_TAB);
       break;
     case 18:
-      Keyboard.press('1');
-      Keyboard.press('8');
+      Keyboard.write('1');
+      Keyboard.write('8');
       Keyboard.write(KEY_TAB);
       break;
     case 21:
-      Keyboard.press('1');
-      Keyboard.press('9');
+      Keyboard.write('1');
+      Keyboard.write('9');
       Keyboard.write(KEY_TAB);
       break;
     case 33:
-      Keyboard.press('2');
-      Keyboard.press('0');
+      Keyboard.write('2');
+      Keyboard.write('0');
       Keyboard.write(KEY_TAB);
       break;
     case 34:
-      Keyboard.press('2');
-      Keyboard.press('1');
+      Keyboard.write('2');
+      Keyboard.write('1');
       Keyboard.write(KEY_TAB);
       break;
 
     case 35:
-      Keyboard.press('2');
-      Keyboard.press('2');
+      Keyboard.write('2');
+      Keyboard.write('2');
       Keyboard.write(KEY_TAB);
       break;
     case 36:
-      Keyboard.press('2');
-      Keyboard.press('3');
+      Keyboard.write('2');
+      Keyboard.write('3');
       Keyboard.write(KEY_TAB);
       break;
     case 37:
-      Keyboard.press('2');
-      Keyboard.press('4');
+      Keyboard.write('2');
+      Keyboard.write('4');
       Keyboard.write(KEY_TAB);
       break;
     case 38:
-      Keyboard.press('2');
-      Keyboard.press('5');
+      Keyboard.write('2');
+      Keyboard.write('5');
       Keyboard.write(KEY_TAB);
       break;
     case 39:
-      Keyboard.press('2');
-      Keyboard.press('6');
+      Keyboard.write('2');
+      Keyboard.write('6');
       Keyboard.write(KEY_TAB);
       break;
     case 40:
-      Keyboard.press('2');
-      Keyboard.press('7');
+      Keyboard.write('2');
+      Keyboard.write('7');
       Keyboard.write(KEY_TAB);
       break;
 
     default:
-      Keyboard.press('H');
-      Keyboard.press('I');
-      Keyboard.press('B');
-      Keyboard.press('A');
+      Keyboard.write('H');
+      Keyboard.write('I');
+      Keyboard.write('B');
+      Keyboard.write('A');
   }
+  delay(10);
 }
 void down_arrow(void) {
   Keyboard.write(KEY_DOWN_ARROW); // send a down arrow
